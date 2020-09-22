95cd3eda5ec946f2e1b35c4f08fe8dd33532cd05
victor.alves <victor.alves@bit.com.br>
2020-09-22 20:49:59 -0300

    Primeiro Comit

---
+++ Questão 4/ProvaBimestral/Controllers/EleicaoController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ProvaBimestral.Models;
+
+namespace ProvaBimestral.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EleicaoController : ControllerBase
+    {
+        private static List<Eleicao> _lstEleicoes { get; set; } = new List<Eleicao>();
+
+        [HttpGet]
+        public IEnumerable<Eleicao> Get()
+        {
+            return _lstEleicoes;
+        }
+
+        [HttpGet("{id}")]
+        public Eleicao Get(int id)
+        {
+            return _lstEleicoes.FirstOrDefault(a => a.Id == id);
+        }
+
+        [HttpPost]
+        public Eleicao Post(Eleicao objEleicao)
+        {
+            if (_lstEleicoes.Count > 0)
+                objEleicao.Id = _lstEleicoes.Max(a => a.Id) + 1;
+            else
+                objEleicao.Id = 1;
+
+            _lstEleicoes.Add(objEleicao);
+
+            return objEleicao;
+        }
+    }
+}
---
+++ Questão 4/ProvaBimestral/Models/Eleicao.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+
+namespace ProvaBimestral.Models
+{
+    public class Eleicao
+    {
+        public int Id { get; set; }
+        
+        public string NomeCanditado { get; set; }
+        
+        public string Partido { get; set; }
+        
+        public string Cargo { get; set; }
+
+        public string Foto { get; set; }
+    }
+}
